c765658a39768697a3ca93cee6c1a9a149c418eb
Adam <ahmparker@users.noreply.github.com>
2017-09-20 12:52:00 -0700

    Update
    added shells for methods

--- ApprovalElection.java
+++ ApprovalElection.java
@@ -1,9 +1,20 @@
 
 public class ApprovalElection {
 
+	public ApprovalElection(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+
+	public Object winner() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
