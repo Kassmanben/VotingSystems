6e2370b53797394c48b60d90997a8ab6d417a2b3
Adam <ahmparker@users.noreply.github.com>
2017-09-20 18:15:36 -0700

    Update
    Working

--- ApprovalElection.java
+++ ApprovalElection.java
@@ -7,38 +7,52 @@ public class ApprovalElection {
 	private static final String[] CANDIDATE_NAMES = {"Akiko", "Bob", "Carlos", "Danielle"};
 	
 	/** Number of votes for each candidate. */
-	private int[] votes;
+	private int[] goodVotes;
+	private int[] badVotes;
+	private int badCounter; //tracks the "disapproval" line
 	
 	public ApprovalElection(String directory) {
-		votes = new int[CANDIDATE_NAMES.length]; // new array to count votes for candidates
+		goodVotes = new int[CANDIDATE_NAMES.length]; // new array to count good votes for candidates
+		badVotes = new int[CANDIDATE_NAMES.length]; // new array to count bad votes for candidates
 		File[] ballots = new File(directory).listFiles();
 		for (File f : ballots) {
 			Ballot b = new Ballot(directory + f.getName());
+			badCounter = 6;
 			if (b.size() > 0) {
-				for (int j = 0; j < 4; j++) 
+				for (int j = 0; j < 5; j++) 
 				{
-					int z = j;
-					for (int i = 0; i < CANDIDATE_NAMES.length; i++) {
-						if (b.get(z).equals(CANDIDATE_NAMES[i])) {
-							votes[i]++;
+					if (j < badCounter) {
+						for (int i = 0; i < CANDIDATE_NAMES.length; i++) {
+							if ( (b.get(j).equals(CANDIDATE_NAMES[i])) && (b.get(j)!=("--"))) {
+							goodVotes[i]++;
+							}
+							if ( (b.get(j).equals("--") ) ) {
+								badCounter = j;
+							}
+						}
+					}
+					else {
+						for (int i = 0; i < CANDIDATE_NAMES.length; i++) {
+							if ( (b.get(j).equals(CANDIDATE_NAMES[i])) ) {
+							badVotes[i]++;
+							}
 						}
 					}
 				}
 			}
 		}
 	}
-
-
 	public Object winner() {
 		int highestCount = -1;
 		String result = "Nobody";
-		for (int i = 0; i < votes.length; i++) {
-			if (votes[i] > highestCount) {
+		for (int i = 0; i < goodVotes.length; i++) {		
+				StdOut.println(CANDIDATE_NAMES[i] + " : " + goodVotes[i] + " - " + badVotes[i]);
+			if (goodVotes[i] > highestCount) {
 				result = CANDIDATE_NAMES[i];
-				highestCount = votes[i];
+				highestCount = goodVotes[i];
 			}
 		}
-		return result;
+		StdOut.println(badCounter);
+		return result;	
 	}
-
 }
