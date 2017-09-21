59aa1da2a79ea25f476cdcec8d0241a491f9f6a8
Adam <ahmparker@users.noreply.github.com>
2017-09-20 23:34:08 -0700

    Create

---
+++ BallotTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BallotTest {
+	private Ballot ballot;
+	
+	
+	@Test
+	public void sizeCountedCorrectly() {
+		ballot = new Ballot("elections/election0/ballot0");
+		assertEquals(3, ballot.size());
+	}
+	@Test
+	public void sizeCountedCorrectly2() {
+		ballot = new Ballot("elections/election3/ballot7");
+		assertEquals(5, ballot.size());
+	}
+	@Test
+	public void sizeCountedCorrectly3() {
+		ballot = new Ballot("elections/election1/ballot2");
+		assertEquals(4, ballot.size());
+	}
+
+}
