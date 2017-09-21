d2a380c1dccdca38498ee25dff1e3064634e9e1e
Ben Kassman <kassmanben@gmail.com>
2017-09-20 17:25:00 -0700

    Fixed

--- Ballot.java
+++ Ballot.java
@@ -18,36 +18,41 @@ public class Ballot {
 			String readLine = "";
 			int i=0;
 			while (((readLine = b.readLine()) != null)) {
-				this.candidates[i] = readLine;
-				readLine = b.readLine();
-				i++;
+				readLine=readLine.trim();
+				if(readLine.equals("Akiko")||readLine.equals("Bob")||readLine.equals("Carlos")||readLine.equals("Danielle")){
+					candidates[i]=readLine;
+					i++;
+				}
 			}
+			for(String l:candidates){
+				StdOut.println(l);
+			}
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		}
-	
+	}
 
 	public int size() {
-		int lineCount=0;
-		for (int i=0;i<4;i++){
-			if(!this.candidates.equals(null)){
+		int lineCount = 0;
+		for (int i = 0; i < 4; i++) {
+			if (!this.candidates.equals(null)) {
 				lineCount++;
 			}
 		}
 		return (lineCount);
 	}
-	
-	public String get(int rank){
-		if(this.candidates[rank]==null){
+
+	public String get(int rank) {
+		if (this.candidates[rank] == null) {
 			return "Nobody";
 		}
-		String candidate=this.candidates[rank];
+		String candidate = this.candidates[rank];
 		return candidate;
 	}
 
 	public static void main(String[] args) {
-
+		Ballot b = new Ballot("elections/election3/ballot0");
 	}
 
 }
